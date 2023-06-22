a9933e35ccede98f8e5f771b44e5784d50229cc5
clinton <clinton.wambua@student.moringaschool.com>
2023-06-22 09:23:26 +0300

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates(:content, { :length => { :minimum => 250 } })
+    validates(:summary, { :length => { :maximum => 250 } })
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :validate_clickbait_title
+
+
+
+    private
+
+    def validate_clickbait_title
+      unless title.present? && title.match?(/Won't Believe|Secret|Top \d+|Guess/)
+        errors.add(:title, 'must be sufficiently clickbait-y')
+      end
+    end
 end
